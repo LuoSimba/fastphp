0887348246158aa6e78ffdefb4a01bd0056aff54
simba <simba@com>
2021-06-09 12:15:25 +0800

    MySQL

--- classes/db/MySQL.php
+++ classes/db/MySQL.php
@@ -21,13 +21,30 @@ class MySQL
             3306);
     }
 
+    public function select_db(string $dbname)
+    {
+        $b = $this->link->select_db($dbname);
+
+        if ($b === false)
+            $this->_raise_error();
+    }
+
     public function query(string $sql)
     {
         $rs = $this->link->query($sql);
 
+        // 查询失败时返回 false
+        if ($rs === false)
+            $this->_raise_error();
+
         $rs1 = new ResultSet($rs);
 
         return $rs1;
+    }
+
+    private function _raise_error()
+    {
+        throw new \Exception('MySQL: ' . $this->link->error . ', ' . $this->link->errno);
     }
 }
 
