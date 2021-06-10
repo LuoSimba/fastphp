c2c75034c19008612dc7221347e33922ec684c3a
simba-luo <r.puppy@qq.com>
2021-06-11 02:13:18 +0800

    Response

--- classes/http/Response.php
+++ classes/http/Response.php
@@ -4,5 +4,17 @@ namespace http;
 
 class Response
 {
+    private $content_type = null;
+
+    public function setContentType($content_type)
+    {
+        $this->content_type = $content_type;
+    }
+
+    public function outputHeaders()
+    {
+        if ($this->content_type)
+            header('Content-Type: ' . $this->content_type);
+    }
 }
 
