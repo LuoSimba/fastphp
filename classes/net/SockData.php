b164b7a78de82c900d7afae03cc198f7e450f9d8
simba <simba@com>
2021-06-15 15:29:11 +0800

    net.SockData

--- classes/net/SockData.php
+++ classes/net/SockData.php
@@ -6,7 +6,7 @@ class SockData
     private $id;
     private $so;
 
-    private $data = '';
+    protected $buffer = '';
 
     private $create_time;
     private $update_time;
@@ -30,11 +30,18 @@ class SockData
         return $this->id;
     }
 
+    public function fd()
+    {
+        return $this->so;
+    }
+
     public function recv()
     {
         $buf = '';
 
         // 尝试读取最多 2048 字节
+        //
+        // 10054 - 远程主机强迫关闭了一个现有的连接
         $ret = socket_recv($this->so, $buf, 2048, 0);
 
         if ($ret === false)
@@ -51,7 +58,7 @@ class SockData
         }
         
         // 保存接收的数据
-        $this->data .= $buf;
+        $this->buffer .= $buf;
     }
 
     /**
@@ -78,7 +85,10 @@ class SockData
      */
     public function resp()
     {
-        $content = "hello java world\n";
+        if ($this->closed())
+            return;
+
+        $content = $this->buffer;
         $ContentLength = strlen($content);
 
         // 如果改用 HTTP/1.0
@@ -86,14 +96,20 @@ class SockData
         // 那么在同时进行两次请求的时候
         // 会非常慢
         $header = "HTTP/1.1 200 OK\r\n"
+            . "Content-Type: text/plain\r\n"
             . "Content-Length: $ContentLength\r\n"
             . "\r\n";
 
-        // send header
-        socket_send($this->so, $header, strlen($header), 0);
+        $this->send($header);
+        $this->send($content);
+    }
 
-        // send content
-        socket_send($this->so, $content, $ContentLength, 0);
+    /**
+     * 发送数据
+     */
+    protected function send($str)
+    {
+        socket_send($this->so, $str, strlen($str), 0);
     }
 }
 
