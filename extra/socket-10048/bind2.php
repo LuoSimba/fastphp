5caac375f9c8d4b85ced4949bb508c95c851af31
simba <simba@com>
2021-06-17 10:42:27 +0800

    socket 10048

---
+++ extra/socket-10048/bind2.php
@@ -0,0 +1,30 @@
+<?php
+// 
+// 将两个 socket 绑定到同一个地址
+// 后绑定的 socket 会失败
+//
+// 但是如果将资源及时销毁，地址就会重新可用
+//
+
+echo "create 2 sockets" . PHP_EOL;
+$s1 = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$s2 = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+echo "bind address 1" . PHP_EOL;
+$b1 = socket_bind($s1, 'localhost', 9999);
+if ($b1 === false)
+{
+    echo "bind address 1 fail" . PHP_EOL;
+}
+
+echo "destroy socket 1" . PHP_EOL;
+unset($s1);
+
+echo "bind address 2" . PHP_EOL;
+$b2 = socket_bind($s2, 'localhost', 9999);
+if ($b2 === false)
+{
+    echo "bind address 2 fail" . PHP_EOL;
+}
+
+
