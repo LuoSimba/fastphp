80cc1581b7e5a447fb745ec8535eacb32166327f
simba-luo <r.puppy@qq.com>
2021-06-21 01:51:19 +0800

    extra: php doesn't support inner class.

---
+++ extra/syntax/inner-class.php
@@ -0,0 +1,15 @@
+<?php
+// 
+// PHP 并不支持内部类的定义
+//
+
+class Car
+{
+    // syntax error, unexpected 'class' (T_CLASS),
+    // expecting function (T_FUNCTION) or const (T_CONST)
+    class Wheel
+    {
+    }
+}
+
+
