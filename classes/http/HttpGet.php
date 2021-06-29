9768bd68237043f3c4359886c299a8bd4611cced
simba-luo <r.puppy@qq.com>
2021-06-30 00:34:40 +0800

    http.HttpGet

--- classes/http/HttpGet.php
+++ classes/http/HttpGet.php
@@ -1,6 +1,19 @@
 <?php
 namespace http;
 
+/**
+ * usage:
+ *
+ * var a = new HttpGet;
+ * a.referer(url2);
+ * a.open(url).send();
+ *
+ * if (a.hasError())
+ *    a.getErrorString();
+ * else
+ *    echo a.getContent();
+ *
+ */
 final class HttpGet
 {
     private $ch;
